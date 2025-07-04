2442279e7f10ec701f3591bc136b0717e3765966
Adorem235 <tmbaya91@gmail.com>
2025-07-04 20:18:39 +1000

    Website skeleton with files prepared for navbar and routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-700">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Welcome to Bluepixel Studios</h1>
+      <p className="mt-4 text-gray-700">Creating immersive and engaging websites.</p>
     </div>
-  )
-}
+  );
+}  
