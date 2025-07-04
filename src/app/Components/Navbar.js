e1607e073f10e3bc3e6753a8f1e5f1c02800b83e
Adorem235 <tmbaya91@gmail.com>
2025-07-04 22:17:28 +1000

    email js dependencies installed, further general link fixes

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-      <header className=" bg-darkGray shadow-md sticky top-0 z-50">
+      <header className="shadow-md sticky top-0 z-50" style={{ backgroundColor: "#444A5C" }}>
         <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
           <div className="flex items-center space-x-3">
             <Image
@@ -15,11 +15,13 @@ export default function Navbar() {
             <Link href="/Pages/About" className="hover:text-blue-700">About Us</Link>
             <Link href="/Pages/Pricing" className="hover:text-blue-700">Pricing</Link>
 
-           { /* To be implemented with projects
-            <Link href="/Pages/Projects" className="hover:text-blue-700">Projects Gallery</Link>
-            */}
+            {   /* To be implemented with projects
+                <Link href="/Pages/Projects" className="hover:text-blue-700">Projects Gallery</Link>
+                */
+            }
+
             <Link
-              href="#contact"
+              href="/Pages/Contact"
               className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
             >
               Contact Us
